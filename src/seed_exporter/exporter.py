4246a06d04bf8da72c1401c0ec55d0a8ef6d92bd
virtu <virtu@cryptic.to>
2024-08-01 11:17:55 +0200

    Perform sanity checks on processing results

--- src/seed_exporter/exporter.py
+++ src/seed_exporter/exporter.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 
 from seed_exporter.config import Config
 from seed_exporter.input import InputReader
-from seed_exporter.stats import DataProcessor
+from seed_exporter.processing import DataProcessing
 
 
 @dataclass
@@ -20,11 +20,10 @@ class Exporter:
         log.debug("Starting export...")
         log.debug("Reading input data...")
         input_reader = InputReader(self.conf.crawler_path, self.conf.timestamp)
-        df = input_reader.get_data()
+        df_input = input_reader.get_data()
 
         log.debug("Processing input data...")
-        processor = DataProcessor(df)
-        df2 = processor.process_data()
-        print(df2)
+        df_stats = DataProcessing.process_data(df_input)
+        print(df_stats)
 
         log.info("Outputting data not implemented: returning")
